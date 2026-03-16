b185686901adadbfa9b30035655f5269a25e2ccb
Emanuela Penitente <manu.penitente@gmail.com>
2026-03-16 12:07:34 +0100

    Fix calculation of annual survival probability

--- pertexp_run.m
+++ pertexp_run.m
@@ -32,7 +32,7 @@ eta     = 1 - exp(-eta_ann);% weekly probability of aging out of infants
 % --- Survival probabilities, infants ---
 mu1_ann_per_thousand = 2.57230;
 mu1_ann              = mu1_ann_per_thousand/1000;
-r1_ann               = 1 - mu1_ann_per_thousand/10000;  
+r1_ann               = 1 - mu1_ann;  
 r1                   = r1_ann^(1/52);
 
 % --- Survival probabilities, infants ---
